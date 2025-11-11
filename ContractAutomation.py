eb3aad8f9c5d544aa4265123a7e3a888909a9023
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2025-11-11 13:40:39 -0700

    Create ContractAutomation.py

---
+++ ContractAutomation.py
@@ -0,0 +1,6 @@
+class ContractAutomation:
+    def __init__(self, spacing, client)
+        self.spacing = spacing
+        self.client=client
+    def get_client_name(self.client):
+        return(f"This catering contract is between ContractAutomation and "f{self.client}")
