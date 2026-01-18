56efe4469b5e3e6c34dee6f47245b44a905cced1
dhelms33 <derrickhelms10@gmail.com>
2026-01-18 04:38:11 -0700

    Added contract

---
+++ contract.py
@@ -0,0 +1,10 @@
+class contract:
+    def __init__(self, caterer, client, parties):
+        self.caterer = caterer
+        self.client = client 
+        self.parties = parties
+    def create_contract(self.caterer, self.client):
+        contract = (
+            "This catering contracted is between "
+            + self.caterer + " " + self.client
+        )
