79c49b46bed25e9be91ae2f06906c747057905e2
Dereck Helms <74729959+dhelms33@users.noreply.github.com>
2026-02-10 11:16:14 -0700

    Update URL_Paths.py

--- URL_Paths.py
+++ URL_Paths.py
@@ -1,3 +1,8 @@
 class UrlPaths(KhanAcademy):
     def __init__(self, language_code, subject):
         self.language_code = language_code
+        self.subject = subject
+
+    def make_url(self.language_code, self.subject):
+        url = "https://" + self.language_code + ".KhanAcademy.org/" + self.subject 
+        return url 
