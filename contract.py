1a8172714b52cae632fef78d86e6f7c101b1de0f
dhelms33 <derrickhelms10@gmail.com>
2026-03-10 15:00:12 -0600

    Added convert responses

--- contract.py
+++ contract.py
@@ -4,7 +4,21 @@ class contract:
         self.client = client 
         self.parties = parties
     def create_contract(self.caterer, self.client):
+        client = input("Input your client ")
+        caterer = input("Input your caterer: ")
         contract = (
             "This catering contracted is between "
             + self.caterer + " " + self.client
         )
+        return contract
+    
+    def rounded_responses(response_num):
+        rounded_response = round(rounded_num, 3)
+        return rounded_response
+    
+    def convert_responses(response_num_rounded):
+        converted_response = bool(rounded_responses(response_num_rounded))
+        return("Converted ranking to are you satisfied False = No, True = YES", converted_response)
+    
+    
+        
