2e133d96844dda0b26a80f4d46eb500bae338013
dhelms33 <derrickhelms10@gmail.com>
2026-03-16 19:34:37 -0600

    Added coupon_codes.py

--- CouponCodes.py
+++ CouponCodes.py
@@ -1,12 +1,13 @@
 class CouponCodes:
-    def __init__(self, cart_total, base_price):
+    def __init__(self, cart_total, base_price, shipping_cost):
         self.cart_total = cart_total
         self.base_price = base_price
+        self.shipping_cost = shipping_cost
         
-    def calculate_best_price(self.cart_total):
+    def calculate_best_price(self.cart_total, self.shipping_cost):
         float_cart_total = float(self.cart_total)
         percent_discount = self.base_price - self.base_price * .15
         fixed_discount = self.base_price - 12
-        best_price = min(percent_discount, fixed_discount)
+        best_price = round(min(percent_discount, fixed_discount)+self.shipping_cost,2)
         return(" Your best price + " + str(best_price))
-    #finished part 1
+    #finished part 3
